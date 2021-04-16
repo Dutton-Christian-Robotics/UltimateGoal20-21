dd85e72d3ed068d4046693ba5b7470f5c6cf4477
Matthew Roberts <thew.roberts@gmail.com>
2021-04-16 12:42:34 +0000

    Transitioned configuration properties to new format. Finished implementing hand methods and overall moveTo methods.

--- ChesterBotWobbleArm.java
+++ ChesterBotWobbleArm.java
@@ -9,40 +9,47 @@ public class ChesterBotWobbleArm extends DefenderBotSystem {
     public Servo hand;
 
     ChesterBotWobbleArm(HardwareMap hm, DefenderBotConfiguration config, DefenderBot b) {
-	   super(hm, config, b);
+        super(hm, config, b);
 
-	   arm = hm.dcMotor.get(cbConfig().MOTOR_WOBBLE_ARM_NAME);
-	   hand = hm.servo.get(cbConfig().SERVO_WOBBLE_HAND_NAME);
+        arm = hm.dcMotor.get(configString("WOBBLEARM_MOTOR_NAME"));
+        hand = hm.servo.get(configString("WOBBLEHAND_SERVO_NAME"));
 
-	   arm.setDirection(cbConfig().WOBBLE_ARM_DIRECTION);
-	   arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-	   arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setDirection(configMotorDirection("WOBBLEARM_MOTOR_DIRECTION"));
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-    }
-
-    public ChesterBotConfiguration cbConfig() {
-	   return (ChesterBotConfiguration) configuration;
     }
 
     public void moveArmToGrabPosition() {
-	   arm.setTargetPosition(cbConfig().WOBBLE_ARM_GRAB_POSITION);
-	   arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-	   arm.setPower(cbConfig().WOBBLE_ARM_GRAB_POSITION_POWER);
+        arm.setTargetPosition(configInt("WOBBLEARM_POSITION_GRAB"));
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        arm.setPower(configDouble("WOBBLEARM_POWER_GRAB"));
 
     }
 
     public void moveArmToCarryPosition() {
-	   arm.setTargetPosition(cbConfig().WOBBLE_ARM_CARRY_POSITION);
-	   arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-	   arm.setPower(cbConfig().WOBBLE_ARM_CARRY_POSITION_POWER);
+        arm.setTargetPosition(configInt("WOBBLEARM_POSITION_CARRY"));
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        arm.setPower(configDouble("WOBBLEARM_POWER_CARRY"));
+
     }
 
     public void moveHandToGrabPosition() {
-
+        hand.setPosition(configDouble("WOBBLEHAND_POSITION_GRAB"));
     }
 
     public void moveHandToCarryPosition() {
+        hand.setPosition(1);
+    }
 
+    public void moveToGrabPosition() {
+        moveArmToGrabPosition();
+        moveHandToGrabPosition();
+    }
+
+    public void moveToCarryPosition() {
+        moveArmToCarryPosition();
+        moveHandToCarryPosition();
     }
 
 
