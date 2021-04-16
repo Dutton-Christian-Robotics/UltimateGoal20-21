03b60a78e7d0ee8a03ffceb2fff8e525ab0a61ca
Matthew Roberts <thew.roberts@gmail.com>
2021-04-16 12:43:23 +0000

    Moved drivetrain motor names to new config naming system. Implemented configuration hash table for accessing configuration properties. Added specific methods for returning config properties and casting to required types.

--- DefenderBotConfiguration.java
+++ DefenderBotConfiguration.java
@@ -1,20 +1,65 @@
 package org.firstinspires.ftc.teamcode.dcs15815;
 
 import java.lang.reflect.Field;
+import java.util.Hashtable;
 
 abstract class DefenderBotConfiguration {
-    public String motor_back_left_name = null;
-    public String motor_front_left_name = null;
-    public String motor_front_right_name = null;
-    public String motor_back_right_name = null;
+    protected Hashtable<String, Field> fieldHashtable;
 
-    public String get(String key) {
+    public String DRIVETRAIN_BACKLEFT_MOTOR_NAME = null;
+    public String DRIVETRAIN_FRONTLEFT_MOTOR_NAME = null;
+    public String DRIVETRAIN_FRONTRIGHT_MOTOR_NAME = null;
+    public String DRIVETRAIN_BACKRIGHT_MOTOR_NAME = null;
+
+    DefenderBotConfiguration() {
+	   fieldHashtable = new Hashtable<>();
+	   for (Field f : DefenderBotConfiguration.class.getDeclaredFields()) {
+		  fieldHashtable.put(f.getName(), f);
+	   }
+    }
+
+    public Object get(String key) {
+	   Field f = fieldHashtable.get(key);
 	   try {
-		  Field f = DefenderBotConfiguration.class.getDeclaredField(key);
-		  return (String) f.get(this);
+		  return f.get(this);
 	   } catch (Exception e) {
 		  return "";
 	   }
+    }
 
+    public String getString(String key) {
+	   Field f = fieldHashtable.get(key);
+	   try {
+		  return f.get(this).toString();
+	   } catch (Exception e) {
+		  return "";
+	   }
+    }
+
+    public int getInt(String key) {
+	   Field f = fieldHashtable.get(key);
+	   try {
+		  return f.getInt(this);
+	   } catch (Exception e) {
+		  return -1;
+	   }
+    }
+
+    public double getDouble(String key) {
+	   Field f = fieldHashtable.get(key);
+	   try {
+		  return f.getDouble(this);
+	   } catch (Exception e) {
+		  return -1;
+	   }
+    }
+
+    public long getLong(String key) {
+	   Field f = fieldHashtable.get(key);
+	   try {
+		  return f.getLong(this);
+	   } catch (Exception e) {
+		  return -1;
+	   }
     }
 }
